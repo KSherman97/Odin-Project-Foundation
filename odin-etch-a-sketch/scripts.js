49129f9bd06f6faa62142378e96729d6e1101c12
KSherman97 <22063898+KSherman97@users.noreply.github.com>
2026-01-18 13:59:12 -0500

    Added basic etch a sketch grid

--- odin-etch-a-sketch/scripts.js
+++ odin-etch-a-sketch/scripts.js
@@ -7,6 +7,38 @@
  * a virtual etch-a-sketch purely using JS and CSS
  * 
  * consists of a 16x16 grid of square divs that will use hover event listener
- * to make a trail following the user mouse movement
+ * to make a trail following the user mouse movements
  * 
+ * 
+ * update: after making the 16x16 functional, I decided to go ahead and add a 100x100 pixel grid
  */
+
+function generateBoard(){ 
+    const boardContainer = document.querySelector("#game-board");
+    // put in an array of board dims?
+    const rows = 100;
+    const cols = 100;
+
+    // generate a 16 x 16 grid
+    // primary issue is how we will identify each grid piece?
+    // consider we can assign a class value of x-1 y-1 though x-16 y-16
+
+    let currentX = 1;
+    let currentY = 1;
+
+    for(let y = currentY; y <= rows; y++) {
+        for(let x = currentX; x <= cols; x++) {
+            let pixel = document.createElement('div');
+            pixel.classList.add("grid-item");
+            // pixel.textContent = x + " " + y;
+
+            pixel.addEventListener('mouseenter', () => {
+                pixel.style.cssText = "background-color: black;";
+            });
+
+            boardContainer.appendChild(pixel);
+        }
+    }
+}
+
+generateBoard();
