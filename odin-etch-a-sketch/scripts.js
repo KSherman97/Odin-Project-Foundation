20a2fff5777860a85e6b94d639d66ed9a5543baf
KSherman97 <22063898+KSherman97@users.noreply.github.com>
2026-01-18 01:13:27 -0500

    Added template for EtchASketch project

---
+++ odin-etch-a-sketch/scripts.js
@@ -0,0 +1,12 @@
+/**
+ * @author: Kyle Sherman
+ * @date: 01/18/2026
+ * @description
+ * This is a foundational project for The Odin Project.
+ * The purpose is to show advanced DOM manipulation by making
+ * a virtual etch-a-sketch purely using JS and CSS
+ * 
+ * consists of a 16x16 grid of square divs that will use hover event listener
+ * to make a trail following the user mouse movement
+ * 
+ */
